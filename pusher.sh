43cc3670a09f893f8dcbc757b613e3bdea8394c2
SubhadityaMukherjee <msubhaditya@gmail.com>
2021-06-06 23:29:39 +0400

    code_revamp

--- pusher.sh
+++ pusher.sh
@@ -1,8 +1,8 @@
 black "."
 isort .
-pdoc --force --html -o docs airJugaad
-mv docs/airJugaad/index.html docs/index.md
-mv docs/airJugaad/* docs/
+pdoc --force --html -o docs airjugaad
+mv docs/airjugaad/index.html docs/index.md
+mv docs/airjugaad/* docs/
 if [[ ! -z $1 ]]; then
         git add . && git commit -m $1 && git push
 fi
